e8929c222cee5f3abd827d95fcad34fae444f5a9
duongquocduy2006-netizen <duongquocduy2006@gmail.com>
2026-01-22 22:12:39 +0700

    Add files via upload

---
+++ ShoeStore-JAVA5-ASM/src/main/java/com/ShoeStore/ShoeStoreJava5AsmApplication.java
@@ -0,0 +1,13 @@
+package com.ShoeStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeStoreJava5AsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoeStoreJava5AsmApplication.class, args);
+	}
+
+}
